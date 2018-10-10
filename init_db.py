91c0f4588cef1bc193f0a3675a86064901360616
DaxxPL <m.henklewski@gmail.com>
2018-10-10 19:56:34 +0200

    initalized one-user_db

--- init_db.py
+++ init_db.py
@@ -1,5 +1,22 @@
 from sqlalchemy import create_engine
+from main import db
+from models import User
+import os
 
 
 def db_start():
-    engine = create_engine('sqlite:///tmp/test/db', convert_unicode=True)
+    if os.path.exists('test.db'):
+        os.remove('test.db')
+    engine = create_engine('sqlite:///tmp/test/db', convert_unicode=True, echo = True)
+    db.create_all()
+    db.session.commit()
+    user = User()
+    user.name = 'Mikołaj'
+    user.password = 'hasło'
+    user.email = 'mik89123@o2.pl'
+    db.session.add(user)
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    db_start()
