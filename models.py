c508a294d4b297ba1b1163142a867d98e7118b3e
DaxxPL <m.henklewski@gmail.com>
2018-10-10 18:24:14 +0200

    created simple User model

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_login import UserMixin
+from main import db
+from sqlalchemy import Column
+from sqlalchemy.types import Integer, Boolean, String
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'Users'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    active = Column(Boolean, default=True)
+    name = Column(String(20), nullable=False)
+    email = Column(String(200), unique=True, nullable=False)
+    password = Column(String(200), nullable=False)
+
+    def is_active(self):
+        return self.active
